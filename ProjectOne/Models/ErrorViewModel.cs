5371af9f8fd66c6a2810527475ba43caca6ed3c1
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-14 02:57:44 -0500

    initial commit

---
+++ ProjectOne/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectOne.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
