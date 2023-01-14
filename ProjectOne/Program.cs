38c4a13ecda66f4c4cc4ec1af141ed6c8104ac8c
Shambhuraj Wadghule <wadghuleshambhuraj@gmail.com>
2023-01-14 03:49:49 -0500

    changed controller to `HelloWorld`

--- ProjectOne/Program.cs
+++ ProjectOne/Program.cs
@@ -22,7 +22,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=HelloWorld}/{action=Index}/{id?}");
 
 app.Run();
 
